584d6d55595a02c64c139c6c74cb8ce74007b713
BuffaloBingo <hayden.f.dorf@gmail.com>
2023-10-05 11:31:52 -0700

    adding button function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
 function arch(){
-alert("hi")
+    document.getElementById("arch").style.display="block";
+    document.getElementById("main").style.display="none";
+}
+
+function archback(){
+    document.getElementById("arch").style.display="none";
+    document.getElementById("main").style.display="block";
 }
